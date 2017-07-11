3be891c6c457e71a4624d1e20914d2cfcd205108
Natesh <nateshpict@gmail.com>
2017-07-12 02:08:51 +0530

    4:1 mux using structural modelling

---
+++ mux4_1_str/mux4_1_str_top.v
@@ -0,0 +1,11 @@
+/*This is the top level module which interfaces with the internal logic*/
+
+module mux4_1_str_top(
+    input [3:0] X,
+    input [1:0] sel,
+    output Y
+    );
+
+mux4_1_str M1(.a(X), .c(sel), .b(Y)); //Connecting top level module to the internal logic
+
+endmodule
